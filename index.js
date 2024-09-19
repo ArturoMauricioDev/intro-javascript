fe7dfae54e22b5fbf6506ee8bfb2170006cbe268
ArturoMauricioDev <atercerosb@univalle.edu>
2024-09-19 08:19:14 -0400

    fisrt class

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hola mundo");
+const mountNode = document.querySelector("#app");
+
+const parrafo = document.createElement("p");
+const text = document.createTextNode(
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab eveniet necessitatibus tempore hic non aliquid sequi numquam quo enim esse placeat alias provident dolores ratione, quam, dolorum natus praesentium dicta."
+);
+parrafo.appendChild(text);
+
+const card = document.createElement("div");
+card.className = "card";
+
+card.appendChild(parrafo);
+
+mountNode.appendChild(card);
